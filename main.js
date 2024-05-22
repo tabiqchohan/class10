8bd6f0face18cbd4580b437ae2a08889bddcee93
tabiqchohan <160527853+tabiqchohan@users.noreply.github.com>
2024-05-22 10:17:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+/*let name1: string = "hello world";
+for (let i=0; i<=50; i++){
+    console.log(name1)
+}*/
+for (var a = 1; a <= 1000; a++) {
+    console.log("Thank you governor kamran khan tessori sahab");
+}
+// const cars = ["Rolls Royece","Bently","F1","R35 skyline"]
+// for (let i=0; i<=4;i++){
+//     console.log("My Favourite car is"+cars[i]);
+// }
